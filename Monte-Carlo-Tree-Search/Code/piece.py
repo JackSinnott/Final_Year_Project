aff4812ee5b1527451c8a5a4f1408498a08b8487
c00242525@itcarlow.ie <c00242525@itcarlow.ie>
2024-01-15 18:59:33 +0000

    Finally fixed error in movement code and now pieces are moving

--- Monte-Carlo-Tree-Search/Code/piece.py
+++ Monte-Carlo-Tree-Search/Code/piece.py
@@ -24,6 +24,9 @@ class Piece:
     def add_move(self, move):
         self.moves.append(move)
 
+    def clear_moves(self):
+        self.moves = []
+
 #Pawn class
 class Pawn(Piece):
     def __init__(self, color):
